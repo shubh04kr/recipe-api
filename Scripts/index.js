6534e6d3c307d998f9d69b35890deb8226fd30b1
shubham_kumar2 <shubham.kumar@thecodebucket.com>
2021-11-30 10:59:17 +0530

    added CRUD

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,60 +1,163 @@
-const myApp = angular
-  .module("myApp", ["ng-route"])
-  .config("$routeProvider", function ($routeProvider, $locationProvider) {
-    $locationProvider.hashPrefix(""); // add configuration
-    $routeProvider
-      .when("/search", {
-        templateUrl: "recipe-api-main/views/recipeSearch.html",
-        controller: "searchController",
-      })
-      .when("/detail", {
-        templateUrl: "recipe-api-main/views/recipeDetail.html",
-        controller: "myController",
-      })
-      .when("/login", {
-        templateUrl: "recipe-api-main/views/login.html",
-        controller: "./components/login/login.js",
-      })
-      .otherwise({
-        redirectTo: "/search",
-      });
-  })
-  .controller("searchController", function ($scope, $http) {
-    const APP_ID = "d8c55356";
-    const API_ID = "d6116be590dc355c9fdd3824299c54fd";
-    $scope.query = "";
-    $scope.searchRecipe = function (query) {
-      console.log("searchRecipe");
+const app = angular.module("myApp", ["ngRoute"]);
+app
+  .config(["$routeProvider", "$locationProvider", RouteConfig])
+  .controller("searchController", searchController)
+  .controller("detailController", detailController);
 
-      const url = `https://api.edamam.com/api/recipes/v2?app_id=${APP_ID}&app_key=${API_ID}&q=${query}&type=public`;
-      $http
-        .get(url)
-        .then((response) => {
-          console.log(response.data.hits[0].image);
-          // console.log(response.data.hits[0].images.THUMBNAIL);
-          console.log(response.data.hits);
-          $scope.recipes = response.data.hits;
-        })
-        .catch();
-    };
-  })
-  .controller("detailController", function ($scope, $http) {
-    console.log("detailController");
-    const APP_ID = "d8c55356";
-    const API_ID = "d6116be590dc355c9fdd3824299c54fd";
-    $scope.query = "";
-    $scope.searchRecipe = function (query) {
-      console.log("searchRecipe");
+function RouteConfig($routeProvider, $locationProvider) {
+  $routeProvider
+    .when("/", {
+      templateUrl: "./views/home.html",
+    })
+    .when("/search", {
+      templateUrl: "./views/recipeSearch.html",
+      controller: searchController,
+    })
+    .when("/detail/:url", {
+      templateUrl: "./views/recipeDetail.html",
+      controller: detailController,
+    })
+    .when("/add", {
+      templateUrl: "./views/recipeAdd.html",
+      controller: addRecipeController,
+    })
+    .when("/yourRecipes", {
+      templateUrl: "./views/recipeView.html",
+      controller: recipeViewController,
+    })
+    .when("/favorates", {
+      templateUrl: "./views/favorates.html",
+      controller: favorateController,
+    })
+    .otherwise({
+      redirectTo: "/",
+    });
+  // $locationProvider.html5Mode(true);
+}
 
-      const url = `https://api.edamam.com/api/recipes/v2?app_id=${APP_ID}&app_key=${API_ID}&q=${query}&type=public`;
-      $http
-        .get(url)
-        .then((response) => {
-          console.log(response.data.hits[0].image);
-          // console.log(response.data.hits[0].images.THUMBNAIL);
-          console.log(response.data.hits);
-          $scope.recipes = response.data.hits;
-        })
-        .catch();
-    };
+function favorateController($scope, $http) {
+  const url = "http://localhost:3000/favorates";
+  $http.get(url).then((response) => {
+    $scope.recipes = response.data;
   });
+
+  $scope.removeFromFavorate = (id) => {
+    $http({
+      url: `http://localhost:3000/favorates/${id}`,
+      method: "DELETE",
+    }).then((response) => {
+      $http.get(url).then((response) => {
+        $scope.recipes = response.data;
+      });
+    });
+  };
+}
+
+function addRecipeController($scope, $http) {
+  $scope.addRecipe = () => {
+    recipeArray.push({
+      title: $scope.recipe.title,
+      cuisine: $scope.recipe.cuisine,
+    });
+    $http({
+      url: "http://localhost:3000/recipes",
+      method: "POST",
+      data: $scope.recipe,
+    }).then((response) => {
+      console.log("Added new Recipe");
+    });
+  };
+}
+
+function searchController($scope, $http) {
+  const APP_ID = "d8c55356";
+  const API_ID = "d6116be590dc355c9fdd3824299c54fd";
+  $scope.query = "";
+  $scope.nextPageFlag = false;
+
+  $scope.searchRecipe = function (query) {
+    const url = `https://api.edamam.com/api/recipes/v2?app_id=${APP_ID}&app_key=${API_ID}&q=${query}&type=public`;
+    $http
+      .get(url)
+      .then((response) => {
+        $scope.recipes = response.data.hits;
+        $scope.nextPageUrl = response.data._links.next.href;
+        $scope.nextPageFlag = true;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  $scope.addToFavorate = (recipe) => {
+    $http({
+      url: "http://localhost:3000/favorates",
+      method: "POST",
+      data: recipe,
+    }).then((response) => {
+      console.log("Added to favorate");
+    });
+  };
+  $scope.nextPage = (url) => {
+    $http.get(url).then((response) => {
+      $scope.recipes = response.data.hits;
+      $scope.nextPageUrl = response.data._links.next.href;
+      $scope.nextPageFlag = true;
+    });
+  };
+}
+
+function detailController($scope, $http, $routeParams) {
+  const APP_ID = "d8c55356";
+  const API_ID = "d6116be590dc355c9fdd3824299c54fd";
+
+  // $scope.searchSpecificRecipe = (query) {
+
+  const url = `https://api.edamam.com/api/recipes/v2/${$routeParams.url}?app_id=${APP_ID}&app_key=${API_ID}&type=public`;
+
+  $http
+    .get(url)
+    .then((response) => {
+      $scope.recipe = response.data.recipe;
+    })
+    .catch((error) => {});
+  // };
+}
+
+function recipeViewController($scope, $http) {
+  $scope.recipes = "";
+  $scope.editFlag = false;
+  const url = "http://localhost:3000/recipes";
+  $http.get(url).then((response) => {
+    $scope.recipes = response.data;
+  });
+
+  $scope.deleteRecipe = (recipe) => {
+    const url = `http://localhost:3000/recipes/${recipe.id}`;
+    $http({
+      url,
+      method: "DELETE",
+    }).then((response) => {
+      console.log("DELETED");
+      const url = "http://localhost:3000/recipes";
+
+      $http.get(url).then((response) => {
+        $scope.recipes = response.data;
+      });
+    });
+  };
+
+  $scope.editRecipe = (recipe) => {
+    $http({
+      url: `http://localhost:3000/recipes/${recipe.id}`,
+      method: "PATCH",
+      data: recipe,
+    }).then((response) => {
+      console.log("edited");
+      const url = "http://localhost:3000/recipes";
+      $http.get(url).then((response) => {
+        $scope.recipes = response.data;
+      });
+    });
+  };
+}
